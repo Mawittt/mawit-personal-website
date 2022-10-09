23f643da1ce517fb5972655db4f0731ec4429ff6
mawit <jackheaven023@gmail.com>
2022-10-09 15:23:49 +0100

    now i have set a next.js project , and have made some testing

---
+++ pages/test/index.tsx
@@ -0,0 +1,18 @@
+import { NextPage } from "next";
+import React from "react";
+import useTest from "./useTest";
+
+const Test: NextPage = () => {
+    const {testFunction} = useTest()
+	return (
+        <div className="index">
+            <div className="inner-box">
+                <h2 className="header" onClick={testFunction}>
+                    this is a test of my portfolio
+                </h2>
+            </div>
+        </div>
+    );
+};
+
+export default Test;
---
+++ pages/test/useTest.ts
@@ -0,0 +1,21 @@
+import React from "react";
+import internal from "stream";
+
+const useTest = () => {
+	
+    return {testFunction};
+
+    function testFunction(){
+        alert("hello runing my next test function")
+
+
+        function stuff (a : number, b? : string) : string{
+            if(!b) b = ""
+            return a + b 
+        }
+
+        const something = stuff(2)
+    }
+};
+
+export default useTest;
